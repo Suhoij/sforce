a946dbb824d72db15eaf82ab3d676669fcd32c07
amk <a2772@mail.ru>
2013-09-16 16:13:30 +0000

    t

--- sf_exec.php
+++ sf_exec.php
@@ -1,6 +1,6 @@
 <?php
 include "sf_ext.php";
-$sessionId='00DG0000000CkUd!AQ0AQHtb7v7X.MLjW24R6JvPeMQCXAwLkcPiB7DoZa39GuaUDhVHnqQsavHESXUlLGydy2e2zOoUBVpvtMQapJw6TiFfbWSo';
+$sessionId='00DG0000000CkUd!AQ0AQASO7j5Ae1w4EEXNK0SzBcDqAJUh7CiTdDb4Jlp_.XRM7qWOzPy4NBKokqrrOUMIj6295JXBP2ZdlgTqzUCCjXE6UNBJ';
 //$sf_url= 'https://na11.salesforce.com';
 //$sf_url='https://soap.sforce.com/schemas/class/HelperClass';
 $sf_url='https://na11.salesforce.com/services/Soap/class/HelperClass';
