043579972c4918fbc7b62c1fd9c1fc30c105d6ac
amk <a2772@mail.ru>
2013-11-04 17:40:39 +0330

    state-ppt

--- state_ppt.php
+++ state_ppt.php
@@ -5,7 +5,7 @@ try {
  $res=file_get_contents(getcwd().'/'.$filename_arr[0]);
 } catch (Exception $e){
     $error_mes=$e->getMessage();
-    error_log(__FILE__.' '.$error_mes);
+    error_log(__FILE__.' ->state_ppt->'.$error_mes);
     $res=json_encode(array('state'=>'error','job'=>'state-ppt-read','mes'=>$error_mes,'time'=>date("d/m/H/i/s")));
 }
 echo $res;
