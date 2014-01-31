7ed18c45445abc5a14bd4f0cfef85f6412939e52
amk <a2772@mail.ru>
2014-01-31 19:18:30 +0330

    .

---
+++ widgets_config.php
@@ -0,0 +1,10 @@
+<?php
+ $widgets_fields=array(
+ "RichText"=>array("IsActive","Data","Code"),
+ "Chart"   =>array("IsActive","Theme","CustomStyle","ChartType","Title","ShowLegend","Legend","LegendLocation","Width","Height","Data","Min","Max"),
+ "Sortable"=>array("IsActive","Theme","CustomStyle","Labels","Values","ObjectName","FieldName","Data"),
+ "Slider"=>array("IsActive","Theme","CustomStyle","Title","Min","Max","sStep","ObjectName","FieldName"),
+ "Video"=>array("IsActive","Width","Height","VideoSource","VideoId","VideoUrl","Autoplay")
+ );
+
+?>
