f9899ca834c274027007f9e5029953f788a76517
amk <a2772@mail.ru>
2014-02-21 18:41:57 +0330

    preview by token

---
+++ preview-slide.php
@@ -0,0 +1,58 @@
+<?php
+define("PATH_SLIDERS", "preview/ppt/");
+$slide_name=$token=$org_id=$app_id="";
+
+if (isset($_GET['slide_name'])) {
+    $slide_name=$_GET['slide_name'];
+}
+if (isset($_GET['token'])) {
+    $token=$_GET['token'];
+}
+if (isset($_GET['org_id'])) {
+    $org_id=$_GET['org_id'];
+}
+if (isset($_GET['app_id'])) {
+    $app_id=$_GET['app_id'];
+}
+require_once "sftk.php";
+$az_store= new AzureStore();
+$file=getcwd()."/ctmobile.app@2x.png";
+if (($org_id !="") && ($app_id !="")) {
+      if ($az_store->findByOrgApp($org_id,$app_id) ) { //---by org app
+          outputImg('OrgApp');
+      }
+}elseif ($az_store->findByToken($token) ) { //---by token
+     outputImg('token');
+} elseif ($az_store->findByOrgApp($org_id,$app_id)) {//---by org_id,app_id
+     outputImg('token-not-OrgApp-try');
+    exit;
+}
+header("Content-Type: image/png");
+header("Content-Length: " . filesize($file));
+readfile($file);
+//-----------------------------------------------output----------------------
+function outputImg($by_what) {
+  global $az_store,$slide_name,$token;
+    $file=getcwd()."/".PATH_SLIDERS.$az_store->org_id."/".$az_store->app_id."/sliders/".$slide_name;
+    if (file_exists($file)) {
+        $filename = basename($file);
+        $file_extension = strtolower(substr(strrchr($filename,"."),1));
+        error_log(__FUNCTION__.' By '.$by_what.' '.$token.' show file: '.$file);
+        $ctype="image/jpg";
+        switch( $file_extension ) {
+            case "gif": $ctype="image/gif"; break;
+            case "png": $ctype="image/png"; break;
+            case "jpeg":
+            case "jpg": $ctype="image/jpg"; break;
+            default:
+        }
+        header("Content-Type: " . $ctype);
+        header("Content-Length: " . filesize($file));
+        readfile($file);
+        exit;
+     } else {
+        error_log(__FUNCTION__.' no file: '.$file);
+        $file=getcwd()."/ctmobile.app@2x.png";
+    }
+}
+?>
--- sftk.php
+++ sftk.php
@@ -1,8 +1,15 @@
 <?php
 //*** tokens control ***
 //
-//define("PATH_AZURE_PHP", "services/azure-sdk-for-php/");
-define("PATH_AZURE_PHP", "pear/WindowsAzure/");
+$dev='ppthtml2';
+if (gethostname()==$dev) {
+   define("PATH_AZURE_PHP", "pear/WindowsAzure/");
+   //define("PATH_AZURE_PHP", "services/azure-sdk-for-php/");
+} else {
+   define("PATH_AZURE_PHP", "pear/WindowsAzure/");
+
+}
+
 
 //require_once PATH_AZURE_PHP."WindowsAzure/WindowsAzure.php";
 require_once PATH_AZURE_PHP."WindowsAzure.php";
@@ -11,7 +18,7 @@ use WindowsAzure\Common\ServicesBuilder;
 use WindowsAzure\Common\ServiceException;
 use WindowsAzure\Table\Models\Entity;
 use WindowsAzure\Table\Models\EdmType;
-
+ /*
 
 $name='sfclmstorage';
 $key='JtyoTzjNZHBAWVuDQNpN3LaJcPWjc51bMvRT7xG4hZA7vpT6qZTjKoJUU6z8sY2+zsvsGe7j05OupWmumABz5A==';
@@ -22,8 +29,8 @@ echo "Start azure";
 
 $tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
 
-$filter = "org_id eq '00D20000000Caz4EAC'";
-
+//$filter = "org_id eq '00D20000000Caz4EAC'";
+$filter = "token eq '88e3a10944d71ac5fe8739ed'";
 try {
     $result = $tableRestProxy->queryEntities("sftokens", $filter);
 } catch(ServiceException $e){
@@ -31,11 +38,73 @@ try {
     $error_message = $e->getMessage();
     echo $code.": ".$error_message."<br />";
 }
-
 $entities = $result->getEntities();
 var_dump($entities);
 foreach($entities as $entity){
     echo $entity->getPartitionKey().":".$entity->getRowKey()."<br />";
 }
-
+*/
+//-------------------------Azure storage class------------------------------------
+class AzureStore {
+  var $state;
+  var $az_table_proxy;
+  var $az_table_tokens;
+  var $az_table_name;
+  var $org_id='';
+  var $app_id='';
+  function __construct() {
+     $name='sfclmstorage';
+     $key='JtyoTzjNZHBAWVuDQNpN3LaJcPWjc51bMvRT7xG4hZA7vpT6qZTjKoJUU6z8sY2+zsvsGe7j05OupWmumABz5A==';
+     $connectionString  = "DefaultEndpointsProtocol=http;AccountName=$name;AccountKey=$key";
+     $this->az_table_tokens="sftokens";
+     try {
+        $this->az_table_proxy = ServicesBuilder::getInstance()->createTableService($connectionString);
+        $this->state.=';conected';
+     } catch(ServiceException $e){
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        $this->state.=';error-conection';
+        error_log(__FUNCTION__.' '.$this->state."  ".$code.$error_message);
+     }
+  }
+  function findByToken($tk) {
+       $filter = "token eq '$tk'";
+       try {
+          //echo "findByKey <br>filter=$filter";
+          $result = $this->az_table_proxy->queryEntities($this->az_table_tokens, $filter);
+          $entities = $result->getEntities();
+          //var_dump($entities);
+          $this->state.=';table-conected';
+          if (count($entities) >=1 ) {
+              $this->org_id=$entities[0]->getPartitionKey();//$entities[0]->getProperty("token")->getValue();
+              $this->app_id=$entities[0]->getRowKey();
+              return true;
+          } else return false;
+      } catch(ServiceException $e){
+          $code = $e->getCode();
+          $error_message = $e->getMessage();
+          $this->state.='error-conection-table';
+          error_log(__FUNCTION__.' '.$this->state."  ".$code.$error_message);
+          return false;
+      }
+  }
+  function findByOrgApp($org_id,$app_id) {
+         $filter = "PartitionKey eq '$org_id' and RowKey eq '$app_id'";
+         try {
+            $result = $this->az_table_proxy->queryEntities($this->az_table_tokens, $filter);
+            $entities = $result->getEntities();
+            if (count($entities)>=1){
+                $this->org_id=$org_id;
+                $this->app_id=$app_id;
+            }
+            return true;
+         } catch(ServiceException $e){
+            $code = $e->getCode();
+            $error_message = $e->getMessage();
+            $this->state.='error-conection-table-by-org-app';
+            error_log(__FUNCTION__.' '.$this->state."  ".$code.$error_message);
+            return false;
+         }
+  }
+}
 ?>
