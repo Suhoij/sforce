0b657cc9da7bf94f90f199a471990fb9f19dc48a
amk <a2772@mail.ru>
2014-02-26 16:36:36 +0330

    vert fun

--- ruby_sliders_extractor.rb
+++ ruby_sliders_extractor.rb
@@ -2,23 +2,29 @@
   require 'fileutils'
   require 'logger'
 
-  UPLOAD_DIR        ='c:/inetpub/wwwroot/upload/'
-  LOG_DIR           ='c:\\inetpub\\wwwroot\\log\\'
-  
+  UPLOAD_DIR        ='c:/work/salesforce/test/'  #  'c:/inetpub/wwwroot/upload/'
+  LOG_DIR           ='c:/work/salesforce/test/'  #'c:\\inetpub\\wwwroot\\log\\'
+  SLIDERS_DIR       ='c:\\work\\salesforce\\test\\sliders\\'  #'c:\\inetpub\\wwwroot\\preview\\ppt\\00DG0000000CkUdMAK\\a01G000000DTkc0IAD\\sliders\\'
+  #PPT_FILE_NAME     ='00D20000000Caz4EAC_a0E2000000odSfIEAU_ANI-Chicago-December-visit---EK.pptx'
+  PPT_FILE_NAME     ='vertical.ppt'
+
+
   log=Logger.new(LOG_DIR+'logextractor.log')
   log.level = Logger::INFO
 
   ppt = WIN32OLE.new('PowerPoint.Application')
   ppt.visible = true
   ## system "start c:/inetpub/wwwroot/upload/Abbott.pptx"
-  presentation = ppt.Presentations.Open('c:/inetpub/wwwroot/upload/Abbott.pptx');
+  presentation = ppt.Presentations.Open(UPLOAD_DIR+PPT_FILE_NAME);
   #
   sliders_cnt=ppt.ActivePresentation.Slides.Count()
   log.info("SLIDERS_CNT :"+sliders_cnt.to_s)
-  sleep 5
-  sliders_dir = 'c:\\inetpub\\wwwroot\\preview\\ppt\\00DG0000000CkUdMAK\\a01G000000DTkc0IAD\\sliders\\' 
+  sleep 1
+
   for i in 1..sliders_cnt
-  	ppt.ActivePresentation.Slides(i).Export(sliders_dir+"slide_#{i}.jpg", ".jpg", 1024,768)
+    #ppt.ActivePresentation.Slides(i).Export(SLIDERS_DIR+"slide_#{i}.jpg", ".jpg", 2048, 1536)
+    ppt.ActivePresentation.Slides(i).Export("c:\\work\\salesforce\\test\\sliders\\slide_#{i}.jpg", ".jpg")
+    log.info(" slide width=#{ppt.ActivePresentation.PageSetup.SlideWidth}  slide height=#{ppt.ActivePresentation.PageSetup.SlideHeight}")
   end
   ppt.ActivePresentation.Close()
   ppt.quit()
