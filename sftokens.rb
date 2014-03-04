89def9a9dc34c788a53b3a589a8f8811916558cf
amk <a2772@mail.ru>
2014-03-04 14:20:58 +0330

    create tokens org

--- sftokens.rb
+++ sftokens.rb
@@ -12,7 +12,45 @@ SLIDERS_DIR ='c:/inetpub/wwwroot/preview/slide/'
 PPT_DIR     ='c:/inetpub/wwwroot/preview/ppt/'
 
 azure_table_service = Azure::TableService.new
+#------------------------------------methods--------------------------
+def createSlideTokens
+  tokens_cnt=0
+  org_cnt=app_cnt=0
+  bad_dir_arr = [".","..","index.php","ppt_params.json","ppt_params.php","sliders","1","2","css","data","scripts"]
+  Dir.entries(PPT_DIR).each_with_index do |org_id_name,org_index|
+    if File.directory?(PPT_DIR+org_id_name) and ( bad_dir_arr.include?(org_id_name)==false)
+      org_cnt+=1
+      app_cnt=0
+      app_id_arr=Dir.entries(PPT_DIR+org_id_name)
+      app_id_arr.each_with_index do |app_id_name,app_index|
+        if File.directory?(PPT_DIR+"/"+org_id_name+"/"+app_id_name)  and ( bad_dir_arr.include?(app_id_name)==false)
+          app_cnt+=1
+          entity = {:PartitionKey =>org_id_name,:RowKey =>app_id_name,:token=>SecureRandom.hex(12) }
+          azure_table_service.insert_entity("sftokens", entity)
+          tokens_cnt=tokens_cnt+org_cnt+app_cnt;
+          p "Done:#{tokens_cnt}"
+        end
+      end
+      p "-----------------"
+    end
+  end
+  abort
 
+end
+
+def createOrgTokens
+  org_cnt=0
+  bad_dir_arr = [".","..","index.php","ppt_params.json","ppt_params.php","sliders","1","2","css","data","scripts"]
+  Dir.entries(PPT_DIR).each_with_index do |org_id_name,org_index|
+    if File.directory?(PPT_DIR+org_id_name) and ( bad_dir_arr.include?(org_id_name)==false)
+      entity = {:PartitionKey =>org_id_name,:RowKey =>org_id_name,:token=>SecureRandom.hex(12) }
+      azure_table_service.insert_entity("orgtokens", entity)
+      org_cnt+=1
+    end
+  end
+  p "Done "+org_cnt
+end
+#-------------------main-------------------------------------
 if ARGV[0] == "deleteTable"
   p "deleteTable..."
   azure_table_service.delete_table("sftokens")
@@ -31,8 +69,8 @@ if ARGV[0] == "createTable"
 end
 
 if ARGV[0] == "createTokens"
-   createSlideTokens if ARGV[1]=='slide'
-   createOrgTokens   if ARGV[1]=='org'
+   createSlideTokens() if ARGV[1]=='slide'
+   createOrgTokens()   if ARGV[1]=='org'
 end
 
 if ARGV[0] == "updateToken"
@@ -81,41 +119,3 @@ end
 ###print result.inspect
 
 #print token.inspect
-#------------------------------------methods--------------------------
-def createSlideTokens
-  tokens_cnt=0
-  org_cnt=app_cnt=0
-  bad_dir_arr = [".","..","index.php","ppt_params.json","ppt_params.php","sliders","1","2","css","data","scripts"]
-  Dir.entries(PPT_DIR).each_with_index do |org_id_name,org_index|
-    if File.directory?(PPT_DIR+org_id_name) and ( bad_dir_arr.include?(org_id_name)==false)
-      org_cnt+=1
-      app_cnt=0
-      app_id_arr=Dir.entries(PPT_DIR+org_id_name)
-      app_id_arr.each_with_index do |app_id_name,app_index|
-        if File.directory?(PPT_DIR+"/"+org_id_name+"/"+app_id_name)  and ( bad_dir_arr.include?(app_id_name)==false)
-          app_cnt+=1
-          entity = {:PartitionKey =>org_id_name,:RowKey =>app_id_name,:token=>SecureRandom.hex(12) }
-          azure_table_service.insert_entity("sftokens", entity)
-          tokens_cnt=tokens_cnt+org_cnt+app_cnt;
-          p "Done:#{tokens_cnt}"
-        end
-      end
-      p "-----------------"
-    end
-  end
-  abort
-
-end
-
-def createOrgTokens
-  org_cnt=0
-  bad_dir_arr = [".","..","index.php","ppt_params.json","ppt_params.php","sliders","1","2","css","data","scripts"]
-  Dir.entries(PPT_DIR).each_with_index do |org_id_name,org_index|
-    if File.directory?(PPT_DIR+org_id_name) and ( bad_dir_arr.include?(org_id_name)==false)
-       entity = {:PartitionKey =>org_id_name,:RowKey =>org_id_name,:token=>SecureRandom.hex(12) }
-       azure_table_service.insert_entity("orgtokens", entity)
-       org_cnt+=1
-    end
-  end
-  p "Done "+org_cnt
-end
