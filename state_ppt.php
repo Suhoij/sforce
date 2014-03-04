83954cec16c1ca2f232bf781ea24cc6db1e34014
amk <a2772@mail.ru>
2014-03-04 16:55:16 +0330

    state slide - org

--- state_ppt.php
+++ state_ppt.php
@@ -2,16 +2,16 @@
 define('PATH_STATE','log/');
 //$action=$_GET['action'];
 
-//----------------------------- add token ---------------------------------------------
+//----------------------------- add slide token ---------------------------------------------
 if ((isset($_GET['action']))&&($_GET['action']=='tk-add')) {
   if ((isset($_GET['org_id']))&&(isset($_GET['app_id']))) {
-       $org_id=$_GET['org_id'];
+        $org_id=$_GET['org_id'];
         $app_id=$_GET['app_id'];
         require_once "sftk.php";
         $cur_token="0";
         $az_store= new AzureStore();
-         try {
-            $cur_token = $az_store->addToken($org_id,$app_id);
+        try {
+            $cur_token = $az_store->addSlideToken($org_id,$app_id);
         } catch (Exception $e) {
             $cur_token="-2";
             error_log(__FILE__.' ->token->'.$e->getMessage());
@@ -20,7 +20,24 @@ if ((isset($_GET['action']))&&($_GET['action']=='tk-add')) {
         exit;
   }
 }
-//----------------------------- out token ---------------------------------------------
+//----------------------------- out org token   ---------------------------------------------
+if ((isset($_GET['action']))&&($_GET['action']=='tk-org')) {
+    if ((isset($_GET['org_id']))) {
+        $org_id=$_GET['org_id'];
+        require_once "sftk.php";
+        $cur_token="0";
+        $az_store= new AzureStore();
+        try {
+            $cur_token = $az_store->getOrgToken($org_id);
+        } catch (Exception $e) {
+            $cur_token="-2";
+            error_log(__FILE__.' ->token->'.$e->getMessage());
+        }
+        echo $cur_token;
+        exit;
+   }
+}
+//----------------------------- out slide token ---------------------------------------------
 if ((isset($_GET['action']))&&($_GET['action']=='tk')) {
   if ((isset($_GET['org_id']))&&(isset($_GET['app_id']))) {
         $org_id=$_GET['org_id'];
@@ -29,7 +46,7 @@ if ((isset($_GET['action']))&&($_GET['action']=='tk')) {
         $cur_token="0";
         $az_store= new AzureStore();
         try {
-            $cur_token = $az_store->getToken($org_id,$app_id);
+            $cur_token = $az_store->getSlideToken($org_id,$app_id);
         } catch (Exception $e) {
             $cur_token="-2";
             error_log(__FILE__.' ->token->'.$e->getMessage());
@@ -40,8 +57,8 @@ if ((isset($_GET['action']))&&($_GET['action']=='tk')) {
 }
 //------------------------------ out state --------------------------------------------
 try {
- $filename_arr = glob(PATH_STATE."state_ppt.*",GLOB_NOSORT);
- $res=file_get_contents(getcwd().'/'.$filename_arr[0]);
+    $filename_arr = glob(PATH_STATE."state_ppt.*",GLOB_NOSORT);
+    $res=file_get_contents(getcwd().'/'.$filename_arr[0]);
 } catch (Exception $e){
     $error_mes=$e->getMessage();
     error_log(__FILE__.' ->state_ppt->'.$error_mes);
